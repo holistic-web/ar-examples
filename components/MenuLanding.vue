7c81e20e3ca3c44e13bd5a4fa7dab9417a9ef0cc
Michael Fitzhavey <michael.fitzhavey@gmail.com>
2022-06-15 05:18:20 +0000

    chore: move head scripts to be in the main page

--- components/MenuLanding.vue
+++ components/MenuLanding.vue
@@ -111,30 +111,6 @@ const birdComponent = {
 
 export default {
   name: 'MenuLanding',
-
-  head() {
-    return {
-      // Scripts required by the 8th Wall Framework
-      script: [
-        // slightly modified version of A-Frame, which fixes some polish concerns
-        { src: '//cdn.8thwall.com/web/aframe/8frame-1.1.0.min.js' },
-        {
-          src: '//cdn.8thwall.com/web/aframe/aframe-physics-system-4.0.1.min.js',
-        },
-
-        // XR Extras - provides utilities like load screen, almost there, and error handling.
-        // See github.com/8thwall/web/tree/master/xrextras
-        { src: '//cdn.8thwall.com/web/xrextras/xrextras.js' },
-        // Add extras from aframe for animations
-        { src: '//cdn.8thwall.com/web/aframe/aframe-extras-6.1.1.min.js' },
-
-        // 8thWall Web - Replace the app key here with your own app key (only works on authorised domains)
-        {
-          src: '//apps.8thwall.com/xrweb?appKey=zl9iYLs0UnM13G8kugSsXRboJtbC2OJOZWmGeV4dvmWAKMaq1kwdRIa4PTdy4WvWyR05BG',
-        },
-      ],
-    }
-  },
   mounted() {
     this.on8thWallReady()
   },
