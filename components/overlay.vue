1bffdffad943028dc236a24445d3e4d01efbac11
Andrew Lewis <39633492+andrewlew1s@users.noreply.github.com>
2022-06-19 05:53:19 +0000

    feat: up arrow + close

--- components/overlay.vue
+++ components/overlay.vue
@@ -2,10 +2,11 @@
   <div id="overlay" style="z-index: 2">
     <div id="header" class="header">{{ projectName }}</div>
     <div id="footer" class="footer">
-      <p id="footerTitle">A Holistic Project</p>
+      <img id="moreInfo" src="../assets/up-arrow.svg" />
+      <img id="closeBtn" src="../assets/close.svg" />
       <img id="holisticsTag" src="../assets/holistics.svg" />
       <a :href="previousPage">
-        <img id="backBtn" backBtn src="../assets/back-arrow.svg" />
+        <img id="backBtn" src="../assets/back-arrow.svg" />
       </a>
     </div>
   </div>
@@ -20,6 +21,33 @@ export default {
       previousPage: window.location.origin,
     }
   },
+  mounted() {
+    this.upArrow()
+    this.closeBtn()
+  },
+  methods: {
+    upArrow() {
+      const moreInfo = document.getElementById('moreInfo')
+      const footer = document.getElementById('footer')
+      const closeBtn = document.getElementById('closeBtn')
+      console.log('hi')
+      moreInfo.addEventListener('click', function () {
+        console.log('hey')
+        console.log('clicked')
+        footer.style.height = '50vh'
+        footer.style.opacity = '.5'
+        closeBtn.style.visibility = 'visible'
+      })
+    },
+    closeBtn() {
+      const closeBtn = document.getElementById('closeBtn')
+      const footer = document.getElementById('footer')
+      closeBtn.addEventListener('click', function () {
+        footer.style.height = '6vh'
+        footer.style.opacity = '1'
+      })
+    },
+  },
 }
 </script>
 
@@ -33,17 +61,28 @@ export default {
 
   left: 50%;
   transform: translate(-50%, 0);
+  transition: 0.3s;
 
   background-image: url('../assets/galaxy.jpg');
   background-position: center;
   background-repeat: no-repeat;
   background-size: cover;
 }
-#footerTitle {
-  text-align: center;
-  color: white;
-  font-family: 'Nunito', monospace;
-  text-shadow: 0px 0px 5px rgba(0, 0, 0, 0.5);
+#moreInfo {
+  position: absolute;
+  z-index: 5;
+  left: 50%;
+  bottom: 0.5vh;
+  max-width: 20vw;
+}
+#closeBtn {
+  visibility: hidden;
+  position: absolute;
+  z-index: 5;
+  right: 1vh;
+  top: 1vh;
+  max-width: 12vw;
+  height: 50px;
 }
 
 #holisticsTag {
